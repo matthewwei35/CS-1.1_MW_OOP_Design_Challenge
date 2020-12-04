502443d5a6a50ec0dac1dc1ce919fc561507d7d5
Matthew Wei <matthewwei35@gmail.com>
2020-12-03 21:50:47 -0800

    [add] Sandwich class

---
+++ Sandwich.py
@@ -0,0 +1,26 @@
+# ------------
+# Sandwich.py
+# ------------
+
+# Class Sandwich
+class Sandwich():
+  def __init__(self, name):
+    self.name = name
+    self.price = 0.00
+    self.ingredients = list()
+
+  def add_ingredients(self, bread, sauce, filling):
+    self.ingredients.extend([bread, sauce, filling])
+
+  def remove_ingredient(self, name):
+    self.ingredients.remove(name)
+
+if __name__ == "__main__":
+  # If you run this file from the terminal
+  # this block is executed.
+  sandwich = Sandwich("Meatball Sub")
+  sandwich.add_ingredients("French", "Marinara", "Meatballs")
+  print(sandwich.ingredients)
+  sandwich.remove_ingredient("Meatballs")
+  print(sandwich.ingredients)
+  
