97fd71f9ff95eb39a5a17dfe314a6cfabe14c8c1
Matthew Wei <matthewwei35@gmail.com>
2020-12-07 17:25:42 -0800

    [add] Menu class

---
+++ Menu.py
@@ -0,0 +1,29 @@
+# ------------
+# Menu.py
+# ------------
+from Sandwich import Sandwich
+
+# Class Menu
+class Menu:
+  def __init__(self):
+    self.menu_type = "Food"
+    self.menu_list = []
+    self.build_menu()
+
+  def build_menu(self):
+    menu_dict = { "Club Sandwich" : 4.00, "Italian" : 10.99, "Cheese Steak" : 5.99 }
+    for key, value in menu_dict.items():
+      new_sandwich = Sandwich(key, value)
+      self.menu_list.append(new_sandwich)
+
+  def remove_sandwich(self, name):
+    self.menu_list.remove(name)
+
+  def add_sandwich(self, name, price):
+    new_sandwich = Sandwich(name, price)
+    self.menu_list.append(new_sandwich)
+
+if __name__ == "__main__":
+  menu = Menu()
+  print(menu.menu_list)
+    
